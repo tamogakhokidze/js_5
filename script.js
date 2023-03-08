d9a55ef4086d973bdc9a3d199113d76684f20900
Tamar Gakhokidze <63312406+tamogakhokidze@users.noreply.github.com>
2023-03-08 19:28:00 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+//task 1
+
+//let array = [12, 25, 3, 6, 8, 14, 7, 23];
+// let newArray = array.map(item => item / 3);
+// console.log(newArray);
+
+//task 2
+
+// let languages = ["html", "css", "javascript", "python", "php"];
+// let newArray = languages.filter((item) => item.length > 3);
+// console.log(newArray);
+
+//task 3
+
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+// let newWords = words.filter (item => item.includes('M','m'));
+
+// console.log(newWords);
+
+//Filter მეთოდის საშუალებით დააბრუნეთ მასივი, რომელიც შედგება ისეთი სიტყვებისგან რომელიც შეიცავს ასო m-ს ან M-ს;
+
+//task 4
+
+// let arr1 = [1, 2];
+// let arr2 = [3, 4];
+// let arr3 = [5, 6];
+// let newArray = arr1.concat(arr2, arr3);
+// console.log(newArray);
+
+//task 5
+
+// let arr = [-1, -2, -3, 4].some((item) => item > 0);
+// console.log(arr);
+
+
+//task 6
+// let array =[23,45,32,5,87,7,3,98];
+// let maxNumber = array.sort((x, y) => y - x)[array.length - 1];
+// console.log(maxNumber);
+
+
+//task 7
+// შექმენით htmlში დივი, მიანიჭეთ id და ჯსდან ამ დივში დაამატეთ შემდეგი html სტრუქტურა:
+// <div class=“wraper”>
+//  <img src=“რაღაცა სურათის მისამართი დაამატეთ” alt=“image”>
+// <h2> image title </h2>
+// </div>
+// H2 ჯს დან დაუმატეთ წითელი ფონტის ფერი და ფროტის ზომა 30px
+
+// შექმენით სამი დივი htmlში, მიანიჭეთ ერთიდაიგივე კლასის სახელი და ყველა დივში დაამატეთ ჯს-დან შემდეგი კოდი:
+// <p class=“text”> hello </p>
+
+
+//task 9
+// let array=[2,15,10,24];
+// let newArray = array.splice(2, 1);
+// console.log(array);
+
+
+
